896f787c27a8184d2dc78d13971bc0d5025bdbd7
imlercj <christoph.imler@gmail.com>
2023-10-04 14:03:06 +0200

    test postgres

--- test_pg.py
+++ test_pg.py
@@ -5,8 +5,6 @@ from datetime import datetime
 
 passing = json.load(open("example.json", "r"))
 
-data_5_min = json.load(open("example_5min.json", "r"))
-
 postgres = Postgres()
 
 # passing
@@ -15,11 +13,3 @@ direction = passing["direction"]
 time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
 last_5min_sum = passing["last_5min_sum"]
 postgres.insert_data_passings([(time, direction, sensor_name, last_5min_sum)])
-
-# 5 min
-sensor_name = data_5_min["sensor_name"]
-time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
-direction = data_5_min["direction"]
-visits = data_5_min["visits"]
-postgres.insert_data_passings_5min([(time, direction, sensor_name, visits)])
-
